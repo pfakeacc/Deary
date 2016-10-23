0001b0d2878f4f6365170c1120f7bf8367ddb303
pfake <pfakeacc@yahoo.com>
2016-10-23 11:40:33 -0700

    eautification

--- routes/emerapi.js
+++ routes/emerapi.js
@@ -13,7 +13,7 @@ exports.index = function(req, res){
 	client.messages.create({
 		to: "+15102039956", 
 		from: "+16692316114",
-		body: "Granny Martha: Hey I am in an Emergency!!",  
+		body: "Granny Martha: I am in an Emergency! Please help. -unit 108",  
 	}, function(err, message) { 
 		console.log(message); 
 	});  
